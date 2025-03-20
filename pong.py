d728929f7883c3f4a489c75bc82bc56f11d0b82d
htcisme <23521673@gm.uit.edu.vn>
2025-03-20 15:32:49 +0700

    hello

--- pong.py
+++ pong.py
@@ -7,7 +7,7 @@ import pyttsx3  # Text-to-speech
 
 # Initialize MediaPipe's Hands model
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.05)
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.15)
 
 # Initialize Pygame and set full-screen mode
 pygame.init()
